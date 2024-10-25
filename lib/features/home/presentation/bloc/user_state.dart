d1a40ec09cf2e88bb1449297dbc93d4f94f2174d
sssloba <slobodan.stojakov@gmail.com>
2024-10-25 12:39:52 +0200

    bloc-user-state

--- lib/features/home/presentation/bloc/user_state.dart
+++ lib/features/home/presentation/bloc/user_state.dart
@@ -1,1 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc_template/features/home/domain/entites/user_entity.dart';
 
+abstract class UserState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class UserInitial extends UserState {}
+
+class UserLoading extends UserState {}
+
+class UserLoaded extends UserState {
+  final List<UserEntity> users;
+  UserLoaded(this.users);
+
+  @override
+  List<Object> get props => [users];
+}
+
+class UserError extends UserState {
+  final String message;
+  UserError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
